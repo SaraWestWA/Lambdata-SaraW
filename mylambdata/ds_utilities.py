51e0e304b2c9d59c7b41add82ae67aa4cf6f8d0d
SaraWestWA <64798747+SaraWestWA@users.noreply.github.com>
2020-09-07 20:37:22 -0700

    Another attempt a pypi test Another attempt a pypi test

--- mylambdata/ds_utilities.py
+++ mylambdata/ds_utilities.py
@@ -1,7 +1,9 @@
-import pandas as pd
+import pandas as pd 
+import numpy as np 
+
 # from sklearn.model_selection import train_test_split
 # from sklearn.datasets import load_wine
-# from pdb import set_trace as breakpoint
+from pdb import set_trace as breakpoint
 
 
 def enlarge(n):
@@ -12,46 +14,53 @@ def enlarge(n):
     return n*100
 
 
+def null_counts(df):
 
+    """
+    Param is a dataframe, can have both catagorical and numeric data
 
+    Function returns a dataframe of counts for  null values and 0.
+    """
+    # surpress warning comparing pd objects to np.nan
+    # warnings.simplefilter(action='ignore', category=FutureWarning)
+    df = df.applymap(lambda x: x.strip() if type(x) == str else x)
+    columns = list(df.columns)
+    n_c = []
+    z_c = []
+    q_c = []
+    m_c = []
+    for i in columns:
+        nan_count = df[i].isnull().sum()
+        zero_count = (sum(df[i] == '0')+sum(df[i] == 0))
+        q_mark_count = sum(df[i] == '?')
+        missing_count = (sum(df[i] == '')+sum(df[i] == None))
+        n_c.append(nan_count)
+        z_c.append(zero_count)
+        q_c.append(q_mark_count)
+        m_c.append(missing_count)
+        null_count = pd.DataFrame(data=(n_c, z_c, q_c, m_c),
+                    index=['NaN', 0, '?', 'Missing'], columns=columns)
+        return (null_count)
 
-"""
-null_counts examines a dataframe and returns another dataframe with
-counts for various null values as well as zeros.
-"""
-
-def null_counts (df):
-  # surpress warning comparing pd objects to np.nan
-  # warnings.simplefilter(action='ignore', category=FutureWarning)
-  df = df.applymap(lambda x: x.strip() if type(x)==str else x)
-  columns = list(df.columns)
-  n_c = []
-  z_c = []
-  q_c = []
-  m_c = []
-  for i in columns:
-    nan_count = df[i].isnull().sum()
-    zero_count = (sum(df[i]=='0')+sum(df[i]==0))
-    q_mark_count = sum(df[i]=='?')
-    missing_count = (sum(df[i]=='')+sum(df[i]==None))
-    n_c.append(nan_count)
-    z_c.append(zero_count)
-    q_c.append(q_mark_count)
-    m_c.append(missing_count)
-  null_count = pd.DataFrame(data=(n_c,z_c,q_c,m_c), index = ['NaN',0, '?', 'Missing'], columns = columns)
-  return (null_count)
-
-"""
-clean_frame removes leading & trailing spaces, replaces '?', '', and empty cells
-with NaN and changes dtype to float64 if possible.
-"""
 
 def clean_frame(df):
-  df = df.applymap(lambda x: x.strip() if type(x)==str else x)
-  df = df.applymap(lambda x: np.nan if type(x)==str and x=='' or x== None or x=='?' else x)
-  df = df.apply(pd.to_numeric,errors='ignore') 
-  return (df)
+
+    """
+    Param is a dataframe, can have both catagorical and numeric data
+
+    Function returns the dataframe with leading and trailing zeros removed;
+    '?','', and empty cells replaced with NaN, dtype changed to float
+    if possible.
+    """
+
+    df = df.applymap(lambda x: x.strip() if type(x) == str else x)
+    df = df.applymap(lambda x: np.nan if type(x) == str and x == ''or
+                     x == None or x == '?' else x)
+    df = df.apply(pd.to_numeric, errors='ignore')
+    return (df)
 
 if __name__ == '__main__':
     print(enlarge(8))
+
     
+
--- mylambdata/test_functions.py
+++ mylambdata/test_functions.py
@@ -1,3 +1,17 @@
 from mylambdata.ds_utilities import enlarge
+from mylambdata.ds_utilities import count_nulls
 
-print(enlarge(9))
+data = ([[1, '', 1, 4, np.nan, 6, '0', 2],
+            [2, 2, 1, 0, 1, 6, 6, 2],
+            ['0', 'x', '? ', 'x', '  ', 0, 'x'],
+            [np.nan, '?', '? ', 'c ', ' x', ' 0 ', ],
+            [.4, .5, .35, ' ?', np.nan, .55, ],
+            [5, .55, 0, .5, .2, .4, .0, .6]
+             ])
+
+names = ['int_a', 'int_b', 'str_a', 'str_b', 'fl_a', 'fl_b']
+
+df = pd.DataFrame(data, index=names).T
+
+nulls = null_counts(df)
+print(nulls)
