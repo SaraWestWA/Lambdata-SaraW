b5073d14078dd8b2e21480f90d48e0b0c09622a5
SaraWestWA <64798747+SaraWestWA@users.noreply.github.com>
2020-09-08 12:43:24 -0700

    Lecture 2 add 3 way split, no branch

--- mylambdata/ds_utilities.py
+++ mylambdata/ds_utilities.py
@@ -1,8 +1,8 @@
 import pandas as pd 
-import numpy as np 
+import numpy as np
 
-# from sklearn.model_selection import train_test_split
-# from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_wine
 from pdb import set_trace as breakpoint
 
 
@@ -61,10 +61,59 @@ def null_counts(df):
                     index=['NaN', 0, '?', 'Missing'], columns=columns)
     return (null_count)
 
+def train_validation_test_split(df, features, target,
+                                train_size=0.7, val_size=0.1,
+                                test_size=0.2, random_state=None,
+                                shuffle=True):
+    '''
+    This function is a utility wrapper around the Scikit-Learn train_test_split that splits arrays or 
+    matrices into train, validation, and test subsets.
+    Args:
+        df (Pandas DataFrame) Dataframe with code.
+
+        X (list) : A list of features.
+        y (string): A string with target column.
+
+        FUTURE DATA
+        # X (Numpy array or DataFrame): This is a dataframe (matrix) with features.
+        # y (Numpy array or DataFrame): This is a pandas Series (vector) with target.
+
+        train_size (float or int): Proportion of the dataset to include in the train split (0 to 1).
+        val_size (float or int): Proportion of the dataset to include in the validation split (0 to 1).
+        test_size (float or int): Proportion of the dataset to include in the test split (0 to 1).
+        random_state (int): Controls the shuffling applied to the data before applying the split for reproducibility.
+        shuffle (bool): Whether or not to shuffle the data before splitting
+
+    Returns:
+        Train, test, and validation dataframes for features (X) and target (y). 
+    '''
+    '''
+    # X_train_val, X_test, y_train_val, y_test = train_test_split(
+    #     self.X, self.y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+    '''
+    X = df[features]
+    y = df[target]
 
 
-# if __name__ == '__main__':
-# print(enlarge(8))
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+
+    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
+        random_state=random_state, shuffle=shuffle)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+if __name__ == '__main__':
+    # Simple test for enlarger function
+    # print(enlarge(8))
+
+    # Test for train_validation_test_split function
+    raw_data = load_wine()
+    df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
+    df['target'] = raw_data['target']
+
+    X_train, X_val, X_test, y_train, y_val, y_test = train_validation_test_split(df, features=['ash', 'hue'], target='target')
+    
+    # breakpoint()
 
     
 
