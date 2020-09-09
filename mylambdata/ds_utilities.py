ba2bbce25ce8e7c966be29479912fd6f423875f5
SaraWestWA <64798747+SaraWestWA@users.noreply.github.com>
2020-09-08 18:17:04 -0700

    class is functional

--- mylambdata/ds_utilities.py
+++ mylambdata/ds_utilities.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_wine
 from pdb import set_trace as breakpoint
+from IPython.display import display
 
 
 def enlarge(n):
@@ -60,49 +61,49 @@ def null_counts(df):
                             columns=columns)
     return (null_count)
 
+class My_Data_Splitter():
+    def __init__(self, df, features, target):
+        self.df = df
+        self.features = features
+        self.target = target
+        self.X = df[features]
+        self.y = df[target]
 
-def train_validation_test_split(df, features, target,
+
+    def train_validation_test_split(self,
                                 train_size=0.7, val_size=0.1,
                                 test_size=0.2, random_state=None,
                                 shuffle=True):
-    '''
-    This function is a utility wrapper around the Scikit-Learn train_test_split
-    that splits arrays or matrices into train, validation, and test subsets.
-    Args:
-        df (Pandas DataFrame) Dataframe with code.
 
-        X (list) : A list of features.
-        y (string): A string with target column.
 
-        FUTURE DATA
-        # X (Numpy array or DataFrame): This is a dataframe (matrix) with features.
-        # y (Numpy array or DataFrame): This is a pandas Series (vector) with target.
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
+                    self.X, self.y, test_size=test_size, random_state=random_state,
+                    shuffle=shuffle)
 
-        train_size (float or int): Proportion of the dataset to include in the train split (0 to 1).
-        val_size (float or int): Proportion of the dataset to include in the validation split (0 to 1).
-        test_size (float or int): Proportion of the dataset to include in the test split (0 to 1).
-        random_state (int): Controls the shuffling applied to the data before applying the split for reproducibility.
-        shuffle (bool): Whether or not to shuffle the data before splitting
+        X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val,
+                                    test_size=val_size / (train_size + val_size),
+                                    random_state=random_state, shuffle=shuffle)
 
-    Returns:
-        Train, test, and validation dataframes for features (X) and target (y).
-    '''
-    '''
-    # X_train_val, X_test, y_train_val, y_test = train_test_split(
-        #     self.X, self.y, test_size=test_size, random_state=random_state,
-        #       shuffle=shuffle)
-    '''
-    X = df[features]
-    y = df[target]
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
-    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y,
-                test_size=test_size, random_state=random_state, shuffle=shuffle)
+    def print_split_summary(self, X_train, X_val, X_test):
 
-    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val,
-                                test_size=val_size / (train_size + val_size),
-                                random_state=random_state, shuffle=shuffle)
+        print('########################## TRAINING DATA ##########################')
+        print(f'X_train Shape: {X_train.shape}')
+        display(X_train.describe(include='all').transpose())
+        print('')
 
-    return X_train, X_val, X_test, y_train, y_val, y_test
+        print('########################## VALIDATION DATA ##########################')
+        print(f'X_val Shape: {X_val.shape}')
+        display(X_val.describe(include='all').transpose())
+        print('')
+
+        print('########################## TEST DATA ##########################')
+        print(f'X_test Shape: {X_test.shape}')
+        display(X_test.describe(include='all').transpose())
+        print('')
+
+
 
 if __name__ == '__main__':
     # Simple test for enlarger function
@@ -113,7 +114,12 @@ if __name__ == '__main__':
     df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
     df['target'] = raw_data['target']
 
-    X_train, X_val, X_test, y_train, y_val, y_test = train_validation_test_split(df,
-                                    features=['ash', 'hue'], target='target')
+    #  breakpoint()
 
-    # breakpoint()
+    #X_train, X_val, X_test, y_train, y_val, y_test = train_validation_test_split
+        #(df, features=['ash', 'hue'], target='target')
+
+    # Test My_Data_Splitter
+    splitter = My_Data_Splitter(df=df, features=['ash', 'hue'], target='target')
+    X_train, X_val, X_test, y_train, y_val, y_test = splitter.train_validation_test_split()
+    splitter.print_split_summary(X_train, X_val, X_test)
